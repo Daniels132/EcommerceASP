2774a2de6d6da9b33af3a2898fbb3d49f8ec9f8e
RafaelrMelo <rafaelrodmelo26@gmail.com>
2020-10-31 18:50:42 -0300

    Formulario de cadstro finalizado

---
+++ Controllers/PessoaController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Infotec.Models;
+
+namespace Infotec.Controllers
+{
+    public class PessoaController : Controller
+    {
+        // GET: Pessoa
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Pessoa/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Pessoa/Create
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View(new PessoaModel());
+        }
+
+        // POST: Pessoa/Create
+        [HttpPost]
+        public ActionResult Create(PessoaModel pModel)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                pModel.Salvar();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pessoa/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pessoa/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Pessoa/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/PessoaModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Infotec.Models
+{
+    public class PessoaModel
+    {
+        // Campos ou atributos da classe (campos do banco de dados)
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+
+        // Criar CONSTANTE para conexão com banco
+        readonly string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Infotec;Integrated Security=True";
+        
+        public void Salvar()
+        {
+            // Cria conexão com BD:
+            using(SqlConnection sqlCon = new SqlConnection(connectionString))
+            {
+                // Abre a conexão com BD:
+                sqlCon.Open();
+                // Instrução SQL para execução no BD:
+                SqlCommand sqlCmd = new SqlCommand(
+                        "INSERT into Pessoa VALUES(@Nome, @Email, @Senha)",sqlCon
+                    );
+                // Atribuir dados aos campos (ou parâmetros da instrução SQL)
+                sqlCmd.Parameters.AddWithValue("@Nome", Nome);
+                sqlCmd.Parameters.AddWithValue("@Email", Email);
+                sqlCmd.Parameters.AddWithValue("@Senha", Senha);
+                sqlCmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
